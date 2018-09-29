f24cab14a915b00e9d3621c9049bcc15af826e11
agbilotia1998 <iit2016508@iiita.ac.in>
2018-09-29 16:51:15 +0530

    Registers models on django-admin panel

--- CRUD/app/models.py
+++ CRUD/app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+        question = models.CharField(max_length=200)
+        date = models.DateTimeField('date published')
+
+        def __str__(self):
+            return self.question
+
+class Choice(models.Model):
+        question =  models.ForeignKey(Question, on_delete=models.CASCADE)
+        choice = models.CharField(max_length=200)
+        votes  = models.IntegerField(default=2)
+
+        def __str__(self):
+            return self.choice
--- CRUD/app/views.py
+++ CRUD/app/views.py
@@ -1,5 +1,18 @@
 # Create your views here.
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello World")
+    questions = Question.objects.order_by('date')
+    return HttpResponse(questions)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
