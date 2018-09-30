af43466a61089132a9ef810c53034319c62cbdcf
agbilotia1998 <iit2016508@iiita.ac.in>
2018-09-30 20:18:54 +0530

    Uses generic view for question model

--- CRUD/app/urls.py
+++ CRUD/app/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
+from django.conf.urls import url
 
 app_name = 'app'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('questions/', views.IndexView.as_view(), name='getAllQuestions'),
-    path('question/<int:question_id>', views.QuestionView.as_view()),
+    url(r'^question/(?P<pk>[0-9]+)', views.QuestionView.as_view()),
+    #path('question/<int:question_id>', views.QuestionView.as_view()),
     path('question', views.QuestionView.as_view(), name='addQuestion'),
     path('<int:question_id>/choices', views.question_and_choices, name='getAllChoicesForQuestion'),
     path('choice/<int:choice_id>', views.ChoiceView.as_view()),
--- CRUD/app/views.py
+++ CRUD/app/views.py
@@ -7,6 +7,7 @@ from rest_framework import views, status
 from rest_framework.request import Request
 from rest_framework.test import APIRequestFactory
 from rest_framework.decorators import api_view
+from rest_framework import generics
 
 factory = APIRequestFactory()
 request = factory.get('/')
@@ -21,39 +22,42 @@ class IndexView(views.APIView):
         data = QuestionSerializer(questions, many=True)
         return Response(data.data, status=status.HTTP_200_OK)
 
+class QuestionView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
 
-class QuestionView(views.APIView):
-    def get(self, request, question_id):
-        # question_id = request.GET.get('question_id')
-        question = Question.objects.filter(pk=question_id).first()
-        questionData = QuestionSerializer(question)
-
-        return Response(questionData.data, status=status.HTTP_200_OK)
-
-    def post(self, request):
-        serializer = QuestionSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-
-            return Response(serializer.data, status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
-
-    def put(self, request, question_id):
-        question = Question.objects.get(pk=question_id)
-        serializer = QuestionSerializer(question, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-
-            return Response(serializer.data, status.HTTP_201_CREATED)
-
-        return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, question_id):
-        question = Question.objects.get(pk=question_id)
-        question.delete()
-
-        return Response(status=status.HTTP_204_NO_CONTENT)
+# class QuestionView(views.APIView):
+#     def get(self, request, question_id):
+#         # question_id = request.GET.get('question_id')
+#         question = Question.objects.filter(pk=question_id).first()
+#         questionData = QuestionSerializer(question)
+#
+#         return Response(questionData.data, status=status.HTTP_200_OK)
+#
+#     def post(self, request):
+#         serializer = QuestionSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#
+#             return Response(serializer.data, status.HTTP_201_CREATED)
+#
+#         return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+#
+#     def put(self, request, question_id):
+#         question = Question.objects.get(pk=question_id)
+#         serializer = QuestionSerializer(question, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#
+#             return Response(serializer.data, status.HTTP_201_CREATED)
+#
+#         return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, question_id):
+#         question = Question.objects.get(pk=question_id)
+#         question.delete()
+#
+#         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class ChoiceView(views.APIView):
